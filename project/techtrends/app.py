b31f0a1bd29e3b60bd3c7a753374fab523ea9dd5
barmalini18 <44473758+barmalini18@users.noreply.github.com>
2023-07-27 20:25:56 +0200

    Update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -120,4 +120,5 @@ def log_message(msg):
     
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
