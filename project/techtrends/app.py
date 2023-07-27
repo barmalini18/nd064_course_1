ff262ba7ed98fdbd98fe8d93d673fb32a1aabc74
barmalini18 <44473758+barmalini18@users.noreply.github.com>
2023-07-27 19:50:59 +0200

    Update app.py
    Added logging of events:
        An existing article is retrieved. The title of the article should be recorded in the log line.
        A non-existing article is accessed and a 404 page is returned.
        The "About Us" page is retrieved.
        A new article is created. The title of the new article should be recorded in the logline.

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -36,13 +36,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      log_message('A non-existing article is accessed and a 404 page is returned'.format(id=post_id))  
       return render_template('404.html'), 404
     else:
+      log_message('Article "{title}" retrieved.'.format(title=post['title']))  
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log_message('The "About Us" page is retrieved.')
     return render_template('about.html')
 
 # Build the /healthz endpoint for the TechTrends application. The endpoint should return the following response:
@@ -55,7 +58,7 @@ def healthz():
             status=200,
             mimetype='application/json'
     )
-    app.logger.debug(logger_message('Test debug message'))
+    log_message('Test debug message')
     return response
 
 #Build a /metrics endpoint that would return the following:
@@ -66,17 +69,14 @@ def healthz():
 def metrics():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
+    post_count = len(posts)
     response = app.response_class(
-            response=json.dumps({"db_connection_count": connection_count, "post_count": len(posts)}),
+            response=json.dumps({"db_connection_count": connection_count, "post_count": post_count}),
             status=200,
             mimetype='application/json'
     )
     connection.close()
     return response
-
-
-
-
 
 
 # Define the post creation functionality 
@@ -94,11 +94,20 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            log_message('Article "{title}" created.'.format(title=title))
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Extend the TechTrends application to log the following events:
+# An existing article is retrieved. The title of the article should be recorded in the log line.
+# A non-existing article is accessed and a 404 page is returned.
+# The "About Us" page is retrieved.
+# A new article is created. The title of the new article should be recorded in the logline.
+def log_message(msg):
+    app.logger.info('{time} | {message}'.format(
+        time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), message=msg))
+    
 # start the application on port 3111
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port='3111')
